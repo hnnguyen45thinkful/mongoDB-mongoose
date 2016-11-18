5f8d155a6476db8f55dfd12d0a5cadd875b321de
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-17 23:51:02 -0800

    updated the CRUD function for each

--- snippets/snippets.js
+++ snippets/snippets.js
@@ -1,6 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
 
-
 MongoClient.connect('mongodb://localhost/', function(err, db) {
     if (err) {
         console.error(err);
@@ -12,22 +11,76 @@ MongoClient.connect('mongodb://localhost/', function(err, db) {
 
 
 	var collection = db.collection('snippets');
-
+	//Updating the create method function
     var create = function(name, content) {
-        db.close();
-    };
-
+    	var snippet = {
+        	name: name,
+        	content: content
+    	};
+    	collection.insert(snippet, function(err, result) {
+        	if (err) {
+            	console.error("Could not create snippet", name);
+            	db.close();
+            	return;
+        }
+        	console.log("Created snippet", name);
+        	db.close();
+    	});
+	};
+	//Updating the read method function
     var read = function(name) {
-        db.close();
-    };
+    	var query = {
+        	name: name
+    	};
+    	collection.findOne(query, function(err, snippet) {
+        	if (!snippet || err) {
+            	console.error("Could not read snippet", name);
+            	db.close();
+            	return;
+        	}
+        	console.log("Read snippet", snippet.name);
+        	console.log(snippet.content);
+        	db.close();
+    	});
+	};
+	//Updating the read method function
+	//To update documents you use the findAndModify method in Mongo. This modifies the first document returned by a query.
+	var update = function(name, content) {
+    	var query = {
+        	name: name
+    	};
 
-    var update = function(name, content) {
-        db.close();
-    };
+    	var update = {
+        	$set: {content: content}
+    	};
 
-    var del = function(name, content) {
-        db.close();
-    };
+    	collection.findAndModify(query, null, update, function(err, result) {
+        	var snippet = result.value;
+        	if (!snippet || err) {
+            	console.error("Could not update snippet", name);
+            	db.close();
+            	return;
+        	}
+        	console.log("Updated snippet", snippet.name);
+        	db.close();
+    	});
+	};
+	//Finally to delete documents you use the findAndRemove method. This removes the first item which matches a query.
+	var del = function(name, content) {
+    	var query = {
+        	name: name
+    	};
+    	collection.findAndRemove(query, function(err, result) {
+        	var snippet = result.value;
+        	if (!snippet || err) {
+            	console.error("Could not delete snippet", name);
+            	db.close();
+            	return;
+        	}
+        	console.log("Deleted snippet", snippet.name);
+        	db.close();
+    	});
+	};
 
     var main = function() {
         if (process.argv[2] == 'create') {
@@ -49,9 +102,9 @@ MongoClient.connect('mongodb://localhost/', function(err, db) {
     };
 
     main();
-    //"C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe"
 });
 
-:
-w-write
-q-quit
+//:
+//w-write
+//q-quit
+    //"C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe"
